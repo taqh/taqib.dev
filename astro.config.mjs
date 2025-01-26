b36778e651165bb4c122ba3c4a0f48783f4e9bff
Taqib <taqib.ibrahim@gmail.com>
2025-01-26 15:06:24 +0100

    ui: minor ui tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: "https://taqib.vercel.app",
+  site: "https://taqib.dev",
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true }
