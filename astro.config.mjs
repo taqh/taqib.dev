6ac0a6b1c592c47d6d66425f9371e9be75417e5e
Taqib <taqib.ibrahim@gmail.com>
2025-11-11 12:36:38 +0100

    remove trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   integrations: [tailwind(), mdx(), sitemap()],
   site: "https://taqib.dev",
   output: "server",
+  trailingSlash: "never",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
