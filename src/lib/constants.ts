a39e3b757538233ccde59ef98b8c8b2fb7530cea
Taqib <taqib.ibrahim@gmail.com>
2026-04-24 00:16:42 +0100

    chore: add fable to projects

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -12,4 +12,12 @@ export const PROJECTS = [
       live: "https://marblecms.com?ref=taqib.dev",
     },
   },
+  {
+    title: "Fable",
+    description: "Transform documents to audio podcasts",
+    links: {
+      repo: "https://helloworld.ng/fable",
+      live: "https://fablepod.com?ref=taqib.dev",
+    },
+  },
 ];
